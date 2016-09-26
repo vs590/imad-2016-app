50bff5ea2128a5be714e68f1c9f66904741aee8d
Vyom Saxena <vs590@snu.edu.in>
2016-09-27 02:29:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,3 @@
 console.log('Loaded!');
-var element=document.getelementById('maintext');
-        element.innerHtml="new text";
+var element=document.getElementById('maintext');
+        element.innerHTML="new text";
